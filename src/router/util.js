1dc336b1ca3a774c30a3cea75bf78a1a42ab21c5
P-iknow <apricotsoul@gmail.com>
2019-09-09 16:10:18 +0900

    util 수정

--- src/router/util.js
+++ src/router/util.js
@@ -1,4 +1,5 @@
 import qs from 'querystringify'
+import { isNumericLiteral } from '@babel/types';
 
 export function locationToRoute(location) {
   // location comes from the history package
@@ -7,4 +8,38 @@ export function locationToRoute(location) {
     hash: location.hash,
     query: qs.parse(location.search)
   };
+}
+
+export function matchPath(pathname, options) {
+  const { exact = false, path } = options;
+
+  if (!path) {
+    return {
+      path: null,
+      url: pathname,
+      isExact: true
+    }
+  }
+
+  const match = new RegExp(`^${path}`).exec(pathname);
+
+  if (!match) {
+    // 메치된 내용이 없을 경우
+    return null;
+  }
+
+  const url = match[0];
+  const isExact = pathname === url;
+
+  if (exact && !isExact) {
+    // exact 옵션이 true 인데,
+    // 정확하게 일치하지 않는 경우 
+    return null;
+  }
+
+  return {
+    path,
+    url,
+    isExact
+  }
 }
